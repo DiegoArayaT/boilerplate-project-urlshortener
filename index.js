5ebed69761cc5754724fc946640d9a9f7d4ab544
DiegoArayaT <nachox341@gmail.com>
2025-03-20 23:24:42 -0300

    Complete URL Shortener Microservice

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const url = require("url");
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false })); // Permite leer datos de formularios
+
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +21,33 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+let urlsDatabase = [];
+
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+  try {
+    const parsedUrl = new URL(originalUrl);
+    dns.lookup(parsedUrl.hostname, (err) => {
+      if (err) {
+        return res.json({ error: "invalid url" });
+      }
+      const shortUrl = urlsDatabase.length + 1;
+
+      urlsDatabase.push({ original_url: originalUrl, short_url: shortUrl });
+
+      res.json({ original_url: originalUrl, short_url: shortUrl });
+    });
+  } catch (e) {
+    return res.json({ error: "invalid url" });
+  }
+});
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const shortUrl = parseInt(req.params.shorturl);
+  const foundUrl = urlsDatabase.find((entry) => entry.short_url === shortUrl);
+
+  return res.redirect(foundUrl.original_url);
 });
 
 app.listen(port, function() {
